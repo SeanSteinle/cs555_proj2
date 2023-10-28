36679db760490d16b692c0067fa39c7f51f8876c
Sean Steinle <sts137@pitt.edu>
2023-10-28 11:34:00 -0400

    finished compiling neighbors dict

--- network_topology.py
+++ network_topology.py
@@ -1,28 +1,33 @@
 import threading
+from collections import defaultdict
 
 def network_init(filename: str):
-    network_map = read_nodes(filename)
-    threads = spinup_threads(network_map)
+    neighbors_dict = read_nodes(filename)
+    print(neighbors_dict)
+    threads = spinup_threads(neighbors_dict)
 
 def read_nodes(filename: str):
+    #read directly from file
     with open(filename, "r") as f:
         text = f.readlines()
     DV = [line.strip('\n').split(' ') for line in text]
     assert len(DV) == len(DV[0]) #should be NxN!
+
+    #get list of neighbor triples
     neighbor_trips = []
     for node,distance_vector in enumerate(DV):
         for neighbor,weight in enumerate(distance_vector):
             if weight != '0':
                 neighbor_trips.append((node,neighbor,int(weight)))
-    neighbor_dict = dict.fromkeys(range(len(DV)), [])
+
+    #turn neighbor triples into neighbors dictionary
+    neighbor_dict = {}
     for neighbor_trip in neighbor_trips:
-        print(neighbor_trip)
         src,dst,weight = neighbor_trip
-        neighbor_dict[src].append((dst,weight))
-    #do a little more processing here?
-    print(neighbor_dict)
+        neighbor_dict.setdefault(src,[]).append((dst,weight))
+
     return neighbor_dict
 
-def spinup_threads(network_map: list): #network_map is NxN array
+def spinup_threads(neighbors_dict: dict): #network_map is NxN array
     #target function for thread should be a Router() constructor
     pass

8f1f836f04ddf44721d35a3c31c4c76618eb4144
Sean Steinle <sts137@pitt.edu>
2023-10-28 11:25:25 -0400

    I'm seeing the problem here... need to map triplets to keys based on first elem in tuple, but end up throwing away first val!

--- network_topology.py
+++ network_topology.py
@@ -9,16 +9,19 @@ def read_nodes(filename: str):
         text = f.readlines()
     DV = [line.strip('\n').split(' ') for line in text]
     assert len(DV) == len(DV[0]) #should be NxN!
-    neighbors = dict.fromkeys(range(len(DV)), [])
-    print(neighbors)
+    neighbor_trips = []
     for node,distance_vector in enumerate(DV):
         for neighbor,weight in enumerate(distance_vector):
-            if weight != '0' and node == :
-                print(f"{node}-{neighbor}-{weight}")
-                neighbors[node].append((neighbor,weight))
-    print(neighbors)
+            if weight != '0':
+                neighbor_trips.append((node,neighbor,int(weight)))
+    neighbor_dict = dict.fromkeys(range(len(DV)), [])
+    for neighbor_trip in neighbor_trips:
+        print(neighbor_trip)
+        src,dst,weight = neighbor_trip
+        neighbor_dict[src].append((dst,weight))
     #do a little more processing here?
-    return neighbors
+    print(neighbor_dict)
+    return neighbor_dict
 
 def spinup_threads(network_map: list): #network_map is NxN array
     #target function for thread should be a Router() constructor

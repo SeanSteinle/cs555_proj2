b593c559924451eea87c10d77abab43d13e886bf
Sean Steinle <sts137@pitt.edu>
2023-11-14 09:21:39 -0500

    created waiting loop in main. repeatedly sends messages to threads asking if they converged, if not, keeps waiting

--- main.py
+++ main.py
@@ -12,20 +12,35 @@ for thread in threads:
     thread.start()
 
 import time
-time.sleep(3)
+time.sleep(2)
 
 #can now check for end of propogation via "has_updates?" command!
-for router_n in range(len(threads)):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((socket.gethostname(), 50000+router_n))
-    client.sendall(b'has_updates?,0,0')
-    response = client.recvfrom(1024)
-    print(f"router #{router_n} has more updates? {response}")
-    client.close()
+converged = False
+while not converged:
+    print("checking for updates...")
+    time.sleep(2)
+    responses = []
+    for router_n in range(len(threads)):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((socket.gethostname(), 50000+router_n))
+        client.sendall(b'has_updates?,0,0')
+        response = client.recvfrom(1024)
+        responses.append(str(response))
+        print(f"router #{router_n} has more updates? {response}")
+        client.close()
 
-time.sleep(3)
+    #if any of the threads have not converged, we must continue!
+    anyTrue = False
+    for response in responses:
+        if 'True' in response:
+            anyTrue = True
+    if anyTrue: converged = False
+    else: converged = True
+
+time.sleep(2)
 
 #can now end server sockets by issuing "end" commands!
+print(f"closing server sockets...")
 for router_n in range(len(threads)):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((socket.gethostname(), 50000+router_n))
@@ -33,5 +48,6 @@ for router_n in range(len(threads)):
     client.close()
 
 #end threads -- will likely need to issue close condition
+print(f"ending threads...")
 for thread in threads:
     thread.join()
